2ed2b42b7b7e89e401789f2e0620d96a59e56f7c
Gia Edgington <gia.edgington@gmail.com>
2020-04-22 17:15:48 -0400

    Create route and component for search page

---
+++ src/pages/BookLanding/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function BookLanding() {
+
+
+    return(
+        <main>
+            <h1>SEARCH FOR BOOKS HERE</h1>
+        </main>
+    )
+}
+
+export default BookLanding;
