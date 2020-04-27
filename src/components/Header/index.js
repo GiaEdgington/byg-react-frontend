23211b73eabe209213d76336d47c28ccc2876844
Gia Edgington <gia.edgington@gmail.com>
2020-04-27 19:50:34 -0400

    Create Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const Header = () => {
+
+    return (
+        <header>
+            <img src="" alt="logo" className="logo" />
+            <nav className="">
+                    <div className="bookList">
+                        <svg className="">
+                            <use xlinkHref=""></use>
+                        </svg>
+                        <span className="">7</span>
+                    </div>
+                    <div className="signout">
+                        <span className="">Sign Out</span>
+                    </div>
+                </nav>
+            </header>
+    );
+};
+
+export default Header;
