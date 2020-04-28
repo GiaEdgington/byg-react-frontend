449c80bbbbf98501102cc128474428f43ab20901
Gia Edgington <gia.edgington@gmail.com>
2020-04-27 20:43:44 -0400

    Style flex header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
-
+import styles from './Header.module.scss';
 
 const Header = () => {
 
     return (
-        <header>
-            <img src="" alt="logo" className="logo" />
-            <nav className="">
+        <header className={styles.header}>
+            <h2>Before You Go</h2>
+            <nav className={styles.userNav}>
                     <div className="bookList">
-                        <svg className="">
-                            <use xlinkHref=""></use>
-                        </svg>
+                        <span>Book List</span>
                         <span className="">7</span>
                     </div>
                     <div className="signout">
                         <span className="">Sign Out</span>
                     </div>
                 </nav>
-            </header>
+        </header>
     );
 };
 
