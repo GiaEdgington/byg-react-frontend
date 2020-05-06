beddaa6e2ca6a1f252f3b865d6ec8c5f47a75ab2
Gia Edgington <gia.edgington@gmail.com>
2020-05-06 14:06:43 -0400

    Map destination books to retrieve ids for books

--- src/components/Destination/index.js
+++ src/components/Destination/index.js
@@ -6,38 +6,45 @@ import styles from './Destination.module.scss';
 const Destination = (destination) => {
     
     const [destinationBooks, setBooks] = useState([]);
+    const [books, fetchBooks] = useState({});
 
-        const getDestination = async (destination) => {
-        
-            let destinationId = destination.destinationId;
-        //console.log(destinationId);
-            try {
-                let result = await fetch(`http://localhost:3000/destination/${destinationId}`);
-                let resultJson = await result.json();
-                setBooks(resultJson.destination.books);
-            } catch (err) {
-                console.log(err);
-            }
+    const getDestination = async (destination) => {
+    
+        let destinationId = destination.destinationId;
+    //console.log(destinationId);
+        try {
+            let result = await fetch(`http://localhost:3000/destination/${destinationId}`);
+            let resultJson = await result.json();
+            setBooks(resultJson.destination.books);
+        } catch (err) {
+            console.log(err);
+        }
     };
+
+    // const getBooks = async (destinationBooks) => {
+
+    // };
 
     useEffect(() => {  
         getDestination(destination);
     }, []);
 
-    const getBooks = (books) =>  (
+    const getBooks = destinationBooks.map(book => {
+        return (
             <BookCard
-            {...books}
-            key={books.length}
-            books={books}
+            {...book}
+            key={book._id}
+            book={book}
             />
         )
+    });
 
     return (
         <div className={styles.container}>
             <div className={styles.destinationCard}>
                 {/* <h3>{destination.books}</h3> */}
                 <div>
-                    {getBooks(destinationBooks)}
+                    {console.log(destinationBooks)}
                 </div>
             </div>
         </div>
