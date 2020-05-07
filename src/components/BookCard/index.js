7bc88c34e9508c31566b5a31f943b2d2be91574f
Gia Edgington <gia.edgington@gmail.com>
2020-05-06 21:17:44 -0400

    Return unique keys

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -6,8 +6,11 @@ const BookCard = (book) => {
     let bookArr = Object.values(book);
 
     const displayBook = bookArr.map((book) => {
+        //console.log(book);
         return (
-            <h3>{book.title}</h3>
+            <div key={book._id}>
+                <h3>{book.title}</h3>
+            </div>
         )
     })
     return (
