8418eadf88e68a47d69f51168e92c5a053d05ad1
Gia Edgington <gia.edgington@gmail.com>
2020-05-06 20:50:42 -0400

    Modify destination component to include books info data

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -3,26 +3,11 @@ import AppContext from '../../contexts/AppContext';
 import Destination from '../Destination';
 
 const BookCard = (book) => {
-    //const [book, fetchBook] = useState({});
 
-  /*   useEffect(() => {  
-        function getBook(book) {
-            destinationBooks.map(bookId => {
-                let result = fetch(`http://localhost:3000/book/${bookId}`);
-                let resultJson = result.json();
-                fetchBook(resultJson.book);
-            })
-        }
-        Destination.subscribeToBooks(books, getBook);
-        return function cleanup() {
-            Destination.unsubscribeToBooks(books, getBook);
-        };
-    }, []); */
 
     return (
         <div>
-            {/* {console.log(book.book)} */}
-            {/* {console.log(book.title)} */}
+            
         </div>
     )
 };
