f579265761a1558d2bf22183bbbe2f792d03975a
Gia Edgington <gia.edgington@gmail.com>
2020-05-18 21:50:24 -0400

    Style login page/breakpoints

--- src/auth/Login/index.js
+++ src/auth/Login/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
+
+import Header from '../../components/Header';
 import styles from './Login.module.scss';
 
 const Login = () => {
 
     return (
-        <div className={styles.signin}>
-            <form className={styles.form}>
-                <label>Username</label>
-                <input type="text"></input>
-                <label>Password</label>
-                <input type="password"></input>
-                <button type="submit" className={styles.btnlogin}>Log In</button>
-                <span>Dont have an account?</span>
-                <button className={styles.btnSignup}>Create account</button>
-            </form>
-            
+        <div>
+            <Header />
+            <div className={styles.container}>
+                <div className={styles.signin}>
+                    <form className={styles.form}>
+                        <label>Username</label>
+                        <input type="text"></input>
+                        <label>Password</label>
+                        <input type="password"></input>
+                        <button type="submit" className={styles.btnlogin}>Log In</button>
+                        <span>Dont have an account?</span>
+                        <button className={styles.btnSignup}>Create account</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 
