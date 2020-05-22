fa3fbe65aacd9f2b0293fb935e849abb25e2e428
giaedgington <gia.edgington@gmail.com>
2020-05-21 21:47:40 -0400

    Add login functionality

--- src/auth/Login/index.js
+++ src/auth/Login/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AppContext from '../../contexts/AppContext';
 
 import Header from '../../components/Header';
 import styles from './Login.module.scss';
 
 const Login = () => {
+    const { updateUser, updatePassword, logUser} = useContext(AppContext);
+
+    const handleChange = (username) => {
+        updateUser(username);
+    };
+
+    const handlePassword = (userPassword) => {
+        updatePassword(userPassword);
+    }
 
     return (
         <div>
@@ -13,10 +23,10 @@ const Login = () => {
                 <div className={styles.signin}>
                     <form className={styles.form}>
                         <label>Username</label>
-                        <input type="text"></input>
+                        <input type="text" onChange={(e) => handleChange(e.target.value)}></input>
                         <label>Password</label>
-                        <input type="password"></input>
-                        <button type="submit" className={styles.btnlogin}>Log In</button>
+                        <input type="password" onChange={(e) => handlePassword(e.target.value)}></input>
+                        <button type="button" value="submit" onClick={logUser} className={styles.btnlogin}>Log In</button>
                         <span>Dont have an account?</span>
                         <Link to='/signup' className={styles.btnSignup}>Create account</Link>
                     </form>
